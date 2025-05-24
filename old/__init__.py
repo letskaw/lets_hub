6a5b817b4f9bb6120d923a1ce94d1a1049d572cb
Kaw <letskaw@gmail.com>
2025-05-24 21:18:47 +0200

    Delete old directory

--- old/__init__.py
+++
@@ -1,9 +0,0 @@
-'''from .stats1 import stats1
-
-from .stats2 import stats2
-
-from .script import get_list
-from .script import load_data
-from .script import get_day
-
-from .side_bar import sidebar_stats_bq'''
